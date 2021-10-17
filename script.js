8956a24f8403d53b277e3d88aa4c892c59dc222a
Jacob <jhutchinson@drury.edu>
2021-10-17 15:26:52 -0500

    Almost finished project

--- script.js
+++ script.js
@@ -52,27 +52,35 @@ function domLoaded() {
             surveyResults.innerHTML += "-All men should regularly get their blood pressure checked. If you suffer from other cardiovascular issues, ";
             surveyResults.innerHTML += "you should get it checked more frequently. High blood pressure is the biggest risk for heart disease!";
             surveyResults.innerHTML += "<br>";
+            surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "-All men should be checked for prostate cancer!";
             surveyResults.innerHTML += "<br>";
+            surveyResults.innerHTML += "<br>";
+            
 
             if(ageValue == "50-54" || ageValue == "55-59" || ageValue == "60+"){
                 surveyResults.innerHTML += "-As a male over 50 you should start getting annual prostate exams to check for colon cancer.";
+                surveyResults.innerHTML += "<br>";
                 surveyResults.innerHTML += "<br>";
             }
             if(ageValue == "35-39" || ageValue == "40-44" || ageValue == "45-49" || ageValue == "50-54" || ageValue == "55-59" || ageValue == "60+"){
                 surveyResults.innerHTML += "-All men 35 or older should regularly get their cholesterol checked";
                 surveyResults.innerHTML += "<br>";
+                surveyResults.innerHTML += "<br>";
             }
-            if((smokingValue == "on") && (ageValue == "60+")){
+            if((smokingValue == "Yes") && (ageValue == "60+")){
                 surveyResults.innerHTML += "-Smoking men over the age of 60 should be screened for an Abdominal Aortic Aneurysm (AAA).";
+                surveyResults.innerHTML += "<br>";
                 surveyResults.innerHTML += "<br>";
             }
             if(familyBloodPressure == "Yes"){
                 surveyResults.innerHTML += "-Individuals with history or experiencing high blood pressure should get checked for Diabetes.";
                 surveyResults.innerHTML += "<br>";
+                surveyResults.innerHTML += "<br>";
             }
             if(ageValue == "60+"){
                 surveyResults.innerHTML += "-All men, regardless of percieved risks, should regularly be tested for HIV.";
+                surveyResults.innerHTML += "<br>";
                 surveyResults.innerHTML += "<br>";
             }
         }
@@ -86,19 +94,35 @@ function domLoaded() {
                 surveyResults.innerHTML += "-As a female over 40 you should start getting annual mammograms.";
                 surveyResults.innerHTML += "<br>"; 
                 surveyResults.innerHTML += "Females ages 30 and over should have testing for the human papillomavirus (HPV) with their Pap smear.";
+                surveyResults.innerHTML += "<br>"; 
             }
             if(ageValue =="18-24" || ageValue== "25-29" || familyCancer == "yes"){
                surveyResults.innerHTML += "Females at the age of 21 should start Pap smear screening "; 
                surveyResults.innerHTML += "<br>";
                surveyResults.innerHTML += "Between the ages of 21-29, women whose Pap smears are normal only need it repeated every three years."
+               surveyResults.innerHTML += "<br>"; 
             }
             if(ageValue == "30-34" || ageValue == "35-39"){
                 surveyResults.innerHTML += "Females ages 30 and over should have testing for the human papillomavirus (HPV) with their Pap smear.";  
+                surveyResults.innerHTML += "<br>"; 
+            }
+            if(ageValue == "45-49" || ageValue == "50-54" || ageValue == "55-59" || ageValue == "60+"){
+                surveyResults.innerHTML += "Recommended starting age for cholesterol screening is age 45.";
+                surveyResults.innerHTML += "<br>"; 
+                surveyResults.innerHTML += "Females over the age of 45 should be screened for colorectal cancer.";
+                surveyResults.innerHTML += "<br>"; 
             }
         }
+        
+        if(familyCancerValue == "Yes"){
+            surveyResults.innerHTML += "-If your family has a history of certain cancers, it is recomended to get a screening for those cancers.";
+            surveyResults.innerHTML += "<br>";
+        }
 
-        if(drinkingValue == "on"){
+        if(drinkingValue == "Yes"){
+            surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "-For all individuals, excessive use of alcohol can lead to long term effects. These include:";
+            surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "--High blood pressure";
             surveyResults.innerHTML += "<br>";
@@ -111,6 +135,7 @@ function domLoaded() {
             surveyResults.innerHTML += "--Digestive Problems";
             surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "--Cancer of the breast, mouth, throat, esophagus, voice box, liver, colon, and rectum.";
+            surveyResults.innerHTML += "<br>";
             surveyResults.innerHTML += "<br>";
         }
     }
